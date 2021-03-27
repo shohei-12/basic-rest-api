07449f8e95ab7ba818e236c6265ecd47c4066688
shohei-12 <shoheimaeda10@gmail.com>
2021-03-27 15:29:41 +0900

    Write error handling

--- app/app.js
+++ app/app.js
@@ -32,7 +32,11 @@ app.get("/api/v1/users/:id", (req, res) => {
   const id = req.params.id;
 
   db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
-    res.json(row);
+    if (!row) {
+      res.status(404).send({ error: "Not Found!" });
+    } else {
+      res.status(200).json(row);
+    }
   });
 
   db.close();
@@ -51,14 +55,12 @@ app.get("/api/v1/search", (req, res) => {
   db.close();
 });
 
-const run = async (sql, db, res, message) => {
+const run = async (sql, db) => {
   return new Promise((resolve, reject) => {
     db.run(sql, (err) => {
       if (err) {
-        res.status(500).send(err);
-        return reject();
+        return reject(err);
       } else {
-        res.json({ message: message });
         return resolve();
       }
     });
@@ -67,21 +69,28 @@ const run = async (sql, db, res, message) => {
 
 // Create a new user
 app.post("/api/v1/users", (req, res) => {
-  // Connect database
-  const db = new sqlite3.Database(dbPath);
+  if (!req.body.name || req.body.name === "") {
+    res.status(400).send({ error: "ユーザー名が指定されていません。" });
+  } else {
+    // Connect database
+    const db = new sqlite3.Database(dbPath);
 
-  const name = req.body.name;
-  const profile = req.body.profile ? req.body.profile : "";
-  const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : "";
+    const name = req.body.name;
+    const profile = req.body.profile ? req.body.profile : "";
+    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : "";
 
-  run(
-    `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`,
-    db,
-    res,
-    "新規ユーザーを作成しました！"
-  );
+    try {
+      run(
+        `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`,
+        db
+      );
+      res.status(201).send({ message: "新規ユーザーを作成しました。" });
+    } catch (e) {
+      res.status(500).send({ error: e });
+    }
 
-  db.close();
+    db.close();
+  }
 });
 
 // Update user data
@@ -93,18 +102,25 @@ app.put("/api/v1/users/:id", (req, res) => {
 
   // 現在のユーザー情報を取得する
   db.get(`SELECT * FROM users WHERE id=${id}`, (err, row) => {
-    const name = req.body.name ? req.body.name : row.name;
-    const profile = req.body.profile ? req.body.profile : row.profile;
-    const dateOfBirth = req.body.date_of_birth
-      ? req.body.date_of_birth
-      : row.date_of_birth;
+    if (!row) {
+      res.status(404).send({ error: "指定されたユーザーが見つかりません。" });
+    } else {
+      const name = req.body.name ? req.body.name : row.name;
+      const profile = req.body.profile ? req.body.profile : row.profile;
+      const dateOfBirth = req.body.date_of_birth
+        ? req.body.date_of_birth
+        : row.date_of_birth;
 
-    run(
-      `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
-      db,
-      res,
-      "ユーザー情報を更新しました！"
-    );
+      try {
+        run(
+          `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+          db
+        );
+        res.status(200).send({ message: "ユーザー情報を更新しました！" });
+      } catch (e) {
+        res.status(500).send({ error: e });
+      }
+    }
   });
 
   db.close();
@@ -117,12 +133,18 @@ app.delete("/api/v1/users/:id", (req, res) => {
 
   const id = req.params.id;
 
-  run(
-    `DELETE FROM users WHERE id=${id}`,
-    db,
-    res,
-    "ユーザー情報を削除しました！"
-  );
+  db.get(`SELECT * FROM users WHERE id=${id}`, (err, row) => {
+    if (!row) {
+      res.status(404).send({ error: "指定されたユーザーが見つかりません。" });
+    } else {
+      try {
+        run(`DELETE FROM users WHERE id=${id}`, db);
+        res.status(200).send({ message: "ユーザーを削除しました！" });
+      } catch (e) {
+        res.status(500).send({ error: e });
+      }
+    }
+  });
 
   db.close();
 });
