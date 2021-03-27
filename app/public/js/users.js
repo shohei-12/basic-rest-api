573902887601fe360f3b918942cf058a7759882a
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2021-03-27 15:30:03 +0900

    Write error handling (#28)

--- app/public/js/users.js
+++ app/public/js/users.js
@@ -6,6 +6,32 @@ const usersModule = (() => {
   const headers = new Headers();
   headers.set("Content-Type", "application/json");
 
+  const handleError = async (res) => {
+    const resJson = await res.json();
+
+    switch (res.status) {
+      case 200:
+        alert(resJson.message);
+        window.location.href = "/";
+        break;
+      case 201:
+        alert(resJson.message);
+        window.location.href = "/";
+        break;
+      case 400:
+        alert(resJson.error);
+        break;
+      case 404:
+        alert(resJson.error);
+        break;
+      case 500:
+        alert(resJson.error);
+        break;
+      default:
+        alert("何らかのエラーが発生しました。");
+    }
+  };
+
   return {
     fetchAllUsers: async () => {
       const res = await fetch(BASE_URL);
@@ -45,10 +71,7 @@ const usersModule = (() => {
         body: JSON.stringify(body),
       });
 
-      const resJson = await res.json();
-
-      alert(resJson.message);
-      window.location.href = "/";
+      return handleError(res);
     },
     setExistingValue: async (uid) => {
       const res = await fetch(`${BASE_URL}/${uid}`);
@@ -76,10 +99,7 @@ const usersModule = (() => {
         body: JSON.stringify(body),
       });
 
-      const resJson = await res.json();
-
-      alert(resJson.message);
-      window.location.href = "/";
+      return handleError(res);
     },
     deleteUser: async (uid) => {
       const ret = window.confirm("このユーザーを削除しますか？");
@@ -91,10 +111,7 @@ const usersModule = (() => {
           method: "DELETE",
         });
 
-        const resJson = await res.json();
-
-        alert(resJson.message);
-        window.location.href = "/";
+        return handleError(res);
       }
     },
   };
